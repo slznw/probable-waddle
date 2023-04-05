e0380dff1ce79afaffdc96332c69983f368ffc31
Semen <95686959+selezneww@users.noreply.github.com>
2023-04-05 17:09:44 +0800

    Delete addnews.js

--- addnews.js
+++
@@ -1,9 +0,0 @@
-function addnews(){
-	var inputText= document.getElementById("text");
-	localStorage.setItem("text", inputText.value);
-   alert("Успешно добавлено!")
-   }
-function getnews() {
-	localStorage.getItem("text")
-	console.log("Обновлено!");
-}
