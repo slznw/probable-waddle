8210a5654ac00262eba58f6628001048f6fa4d3d
Semen <95686959+selezneww@users.noreply.github.com>
2023-04-08 19:48:55 +0800

    Глобальное обновление
    Полностью изменили дизайн,
    Переписали функции
    Добавили удаление новостей
    Косметические правки

--- news.js
+++ news.js
@@ -1,13 +1,16 @@
 function addnews(){
 	var inputText= document.getElementById("text");
 	localStorage.setItem("text", inputText.value);
-   	alert("Успешно добавлено!")
+	alert("Успешно добавлено!")
    }
 function getnews() {
 	const statusElement = document.getElementById('news-text')
-
 	statusElement.innerText = localStorage.getItem("text")
 }
 function clearfield() {
 	alert("Поле успешно очищено")
+}
+function clearnews() {
+	localStorage.removeItem("text")
+	alert("Новость успешно удалена.")
 }
