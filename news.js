9dcf8458177d0afb0f177421a6543b11226a705b
Semen <95686959+selezneww@users.noreply.github.com>
2023-05-03 09:15:28 +0000

    Update

--- news.js
+++ news.js
@@ -1,4 +1,4 @@
-function news(){
+function news_script1(){
 	var inputText= document.getElementById("text");
 	var inputAuthor= document.getElementById("author");
 	localStorage.setItem("text", inputText.value);
@@ -8,6 +8,30 @@ function news(){
 	const newsAuthor = document.getElementById('news-author')
 	newsText.innerText = localStorage.getItem("text")
 	newsAuthor.innerText = localStorage.getItem("author")
+
+   }
+   function news_script2(){
+	var inputText= document.getElementById("text2");
+	var inputAuthor= document.getElementById("author2");
+	localStorage.setItem("text", inputText.value);
+	localStorage.setItem("author", inputAuthor.value);
+	alert("Успешно добавлено!")
+	const newsText = document.getElementById('news-text2')
+	const newsAuthor = document.getElementById('news-author2')
+	newsText.innerText = localStorage.getItem("text2")
+	newsAuthor.innerText = localStorage.getItem("author2")
+
+   }
+   function news_script3(){
+	var inputText= document.getElementById("text3");
+	var inputAuthor= document.getElementById("author3");
+	localStorage.setItem("text", inputText.value);
+	localStorage.setItem("author", inputAuthor.value);
+	alert("Успешно добавлено!")
+	const newsText = document.getElementById('news-text3')
+	const newsAuthor = document.getElementById('news-author3')
+	newsText.innerText = localStorage.getItem("text3")
+	newsAuthor.innerText = localStorage.getItem("author3")
 
    }
 function clearfield() {
