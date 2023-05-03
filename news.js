77f2896cdef3319171064c68064428094e4e8c98
Semen <95686959+selezneww@users.noreply.github.com>
2023-05-03 09:17:26 +0000

    Update

--- news.js
+++ news.js
@@ -11,27 +11,27 @@ function news_script1(){
 
    }
    function news_script2(){
-	var inputText= document.getElementById("text2");
-	var inputAuthor= document.getElementById("author2");
-	localStorage.setItem("text", inputText.value);
-	localStorage.setItem("author", inputAuthor.value);
+	var inputText2= document.getElementById("text2");
+	var inputAuthor2= document.getElementById("author2");
+	localStorage.setItem("text", inputText2.value);
+	localStorage.setItem("author", inputAuthor2.value);
 	alert("Успешно добавлено!")
-	const newsText = document.getElementById('news-text2')
-	const newsAuthor = document.getElementById('news-author2')
-	newsText.innerText = localStorage.getItem("text2")
-	newsAuthor.innerText = localStorage.getItem("author2")
+	const newsText2 = document.getElementById('news-text2')
+	const newsAuthor2 = document.getElementById('news-author2')
+	newsText2.innerText = localStorage.getItem("text2")
+	newsAuthor2.innerText = localStorage.getItem("author2")
 
    }
    function news_script3(){
-	var inputText= document.getElementById("text3");
-	var inputAuthor= document.getElementById("author3");
-	localStorage.setItem("text", inputText.value);
-	localStorage.setItem("author", inputAuthor.value);
+	var inputText3= document.getElementById("text3");
+	var inputAuthor3= document.getElementById("author3");
+	localStorage.setItem("text", inputText3.value);
+	localStorage.setItem("author", inputAuthor3.value);
 	alert("Успешно добавлено!")
-	const newsText = document.getElementById('news-text3')
-	const newsAuthor = document.getElementById('news-author3')
-	newsText.innerText = localStorage.getItem("text3")
-	newsAuthor.innerText = localStorage.getItem("author3")
+	const newsText3 = document.getElementById('news-text3')
+	const newsAuthor3 = document.getElementById('news-author3')
+	newsText3.innerText = localStorage.getItem("text3")
+	newsAuthor3.innerText = localStorage.getItem("author3")
 
    }
 function clearfield() {
