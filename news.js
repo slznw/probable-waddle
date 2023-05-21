0ed75f0a6848b6a991f316a00f883ab0648fe447
Semen <95686959+selezneww@users.noreply.github.com>
2023-05-21 16:45:12 +0800

    Update news.js

--- news.js
+++ news.js
@@ -1,18 +1,5 @@
 function news_script1(){
 		var inputText= document.getElementById("text");
-	if(inputText != ""){
-				var inputText2= document.getElementById("text");
-		var inputAuthor2= document.getElementById("author");
-		localStorage.setItem("text2", inputText2.value);
-		localStorage.setItem("author2", inputAuthor2.value);
-		alert("Успешно добавлено!")
-		const newsText2 = document.getElementById('news-text2')
-		const newsAuthor2 = document.getElementById('news-author2')
-		newsText2.innerText = localStorage.getItem("text2")
-		newsAuthor2.innerText = localStorage.getItem("author2")
-
-	} else {
-		var inputText= document.getElementById("text");
 		var inputAuthor= document.getElementById("author");
 		localStorage.setItem("text", inputText.value);
 		localStorage.setItem("author", inputAuthor.value);
@@ -21,7 +8,6 @@ function news_script1(){
 		const newsAuthor = document.getElementById('news-author')
 		newsText.innerText = localStorage.getItem("text")
 		newsAuthor.innerText = localStorage.getItem("author")
-	};
    }
    function news_script2(){
 	var inputText2= document.getElementById("text");
